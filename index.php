c4b97f00d6b9e32c71b718a5eac8a53beae7598d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-04-02 12:29:23 -0500

    [+] Crea archivos base de personalización del Tema del sitio

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * El archivo de plantilla principal.
+ *
+ * Este es el archivo de plantilla más genérico en un tema de WordPress
+ * y uno de los dos archivos necesarios para un tema (el otro es style.css).
+ * Se utiliza para mostrar una página cuando nada más específico coincide con una consulta.
+ * Por ejemplo, reúne la página de inicio cuando no existe ningún archivo home.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage JT_ABCServiTodo
+ * @since 1.0.0
+ */
+?>
+<h1>JT-ABCServiTodo</h1>
+<p>index.html</p>
