ba65edb8d6b5d46fa02f69850aa097c3eba27a1a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-04-04 11:03:01 -0500

    [+] Estructura base de archivos del Tema

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * El archivo de plantilla principal.
+ * The main template file
  *
- * Este es el archivo de plantilla más genérico en un tema de WordPress
- * y uno de los dos archivos necesarios para un tema (el otro es style.css).
- * Se utiliza para mostrar una página cuando nada más específico coincide con una consulta.
- * Por ejemplo, reúne la página de inicio cuando no existe ningún archivo home.php
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -13,6 +13,35 @@
  * @subpackage JT_ABCServiTodo
  * @since 1.0.0
  */
+
+get_header();
 ?>
-<h1>JT-ABCServiTodo</h1>
-<p>index.html</p>
+
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		if ( have_posts() ) {
+
+			// Load posts loop.
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/content/content' );
+			}
+
+			// Previous/next page navigation.
+			jtabcservitodo_the_posts_navigation();
+
+		} else {
+
+			// If no content, include the "No posts found" template.
+			get_template_part( 'template-parts/content/content', 'none' );
+
+		}
+		?>
+
+		</main><!-- .site-main -->
+	</section><!-- .content-area -->
+
+<?php
+get_footer();
---
+++ readme.txt
@@ -0,0 +1,23 @@
+=== Detalles del Tema ===
+Nombre del Tema: JT-ABCServiTodo
+URL del Tema: Pendiente.
+Desarrollada por: Pendiente.
+Diseñada por: Pendiente.
+Contribuyentes: Pendiente.
+Requiere mínimo: WordPress 4.9.6
+Probado para: WordPress 5.1
+Stable tag: 0.1
+License: Pendiente por definir
+License URI:
+Tags: one-column, flexible-header, accessibility-ready, custom-colors, custom-menu, custom-logo, editor-style, featured-images, footer-widgets, rtl-language-support, sticky-post, threaded-comments, translation-ready
+
+== Descripción ==
+Tema desarrollado para WordPress como plantilla oficial para el sitio web de ABC ServiTodo.
+Este tema esta basado en el tema "Twenty Nineteen" con la idea de arrastrar la compatibilidad del editor de bloques incluido en la nueva versión de WordPress.
+
+== Resources ==
+Recursos propios de la base "Twenty Nineteen"
+* normalize.css, © 2012-2018 Nicolas Gallagher and Jonathan Neal, MIT
+* Underscores, © 2012-2018 Automattic, Inc., GNU GPL v2 or later
+Otros recursos para desarrollar el tema para dicha organización
+* Gulp 4, Sass
